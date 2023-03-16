5d5fdb7c9edd411aaffe1c57590090b2a3264a99
mirkkajones <81167765+mirkkajones@users.noreply.github.com>
2023-03-16 12:01:02 +0200

    Update 3_VariancePartitioningFigure.R

--- 3_VariancePartitioningFigure.R
+++ 3_VariancePartitioningFigure.R
@@ -8,13 +8,14 @@ load("models_thin_5000_samples_250_chains_4.Rdata")
 m = models[[1]]
 
 #Calculate variation partitioning (fractions of variance explained by the model's fixed and random effects per virus) and barplot the result:
-pdf("VarPart.pdf", width = 6, height = 5)
-par(mar = c(5, 5, 3, 8.5), lwd = 0.1)
-{
   preds = computePredictedValues(m)
   VP = computeVariancePartitioning(m, group = attr(m$X[[1]], "assign"), groupnames = attr(terms(m$XFormula), "term.labels"))
   mycols = tol(nrow(VP$vals))
-  barplot(VP$vals, ylim = c(0,1), cex.lab = 0.8, ylab = "Fractions of variance explained", cex.axis = 0.7, col = mycols, las=2, horiz=FALSE, cex.names = 0.6)
+
+pdf("VarPart.pdf", width = 6, height = 5)
+par(mar = c(5, 5, 3, 8.5), lwd = 0.1)
+{
+  barplot(VP$vals, ylim = c(0,1), cex.lab = 0.8, ylab = "Fractions of explained variance", cex.axis = 0.7, col = mycols, las=2, horiz=FALSE, cex.names = 0.6)
   legend("topright", inset = c(-0.48, 0), xpd=TRUE, legend = rev(c(all.vars(m$XFormula), "Random (plant)", "Random (population)")), fill = rev(mycols), bty = "n", cex = 0.8)
 }
 dev.off()
