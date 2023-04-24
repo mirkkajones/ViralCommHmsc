bb3974dbb1981413cf0d9d146bb31ad83a79f1a8
mirkkajones <81167765+mirkkajones@users.noreply.github.com>
2023-04-24 12:18:46 +0300

    Update BetaIntervals_Supplement.R

--- BetaIntervals_Supplement.R
+++ BetaIntervals_Supplement.R
@@ -9,7 +9,7 @@ betatable = data.frame(rownames(CIs[[1]]),CIs[[1]][, 1], CIs[[2]][, c(3, 1, 5)])
 names(betatable) = c("parameter","mean", "median", "lower_CI", "upper_CI")
 betatable$dataset <- "pooled"
 
-load("MJ_T2_models_thin_5000_samples_250_chains_4.Rdata")
+load("T2_models_thin_5000_samples_250_chains_4.Rdata")
 m = models[[1]]
 mpost = convertToCodaObject(m)
 CIs = summary(mpost$Beta)
@@ -18,7 +18,7 @@ names(betatableT) = c("parameter","mean", "median", "lower_CI", "upper_CI")
 betatableT$dataset <- "T2"
 betatable <- rbind(betatable,betatableT)
 
-load("MJ_T3_models_thin_5000_samples_250_chains_4.Rdata")
+load("T3_models_thin_5000_samples_250_chains_4.Rdata")
 m = models[[1]]
 mpost = convertToCodaObject(m)
 CIs = summary(mpost$Beta)
@@ -27,7 +27,7 @@ names(betatableT) = c("parameter","mean", "median", "lower_CI", "upper_CI")
 betatableT$dataset <- "T3"
 betatable <- rbind(betatable,betatableT)
 
-load("MJ_T4_models_thin_5000_samples_250_chains_4.Rdata")
+load("T4_models_thin_5000_samples_250_chains_4.Rdata")
 m = models[[1]]
 mpost = convertToCodaObject(m)
 CIs = summary(mpost$Beta)
